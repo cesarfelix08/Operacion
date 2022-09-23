bef14c007fd00cd89a190827f98eef629082ac6d
cf384 <cf384@M3>
2022-09-23 16:48:53 -0600

    Agregar archivos de proyecto.

---
+++ Operacion/clase/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacion.clase
+{
+    internal class Form1
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clsuma osuma = new clsuma(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            textBox3.Text = osuma.sumar().ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+
+    
+}
